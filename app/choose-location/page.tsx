320698c706adc5c9579885fb515cd39a5ab442ec
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2023-09-16 14:39:48 +0100

    feat/Radar maps test

--- app/choose-location/page.tsx
+++ app/choose-location/page.tsx
@@ -1,30 +1,67 @@
 "use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import location from "@/public/assets/location.svg";
 import marker from "@/public/assets/marker.svg";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  Autocomplete,
-} from "@react-google-maps/api";
-import { useState } from "react";
-
+import Link from "next/link";
 
 const ChooseLocation = () => {
-const [selectedLocation, setSelectedLocation] = useState({
-  lat: null,
-  lng: null,
-});
+  const [currentLocation, setCurrentLocation] = useState("");
+  const [LAT, setLAT] = useState(0);
+  const [LONG, setLONG] = useState(0);
+  const apiKey = process.env.NEXT_PUBLIC_RADAR_MAPS_API_KEY;
+  const query = "Amule";
 
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "YOUR_API_KEY",
-    libraries: ["places"],
-  });
+  // const apiUrl = `https://api.radar.io/v1/search/autocomplete?query=${encodeURIComponent(query)}&near=${latitude},${longitude}`;
 
-  let autocomplete;
+  const reverseGeoCode = `https://api.radar.io/v1/geocode/reverse?coordinates=${LAT},${LONG}`;
 
-  if (!isLoaded) return <p>Cannot Load Maps...</p>
+  function useCurrentLocation() {
+    getCurrentLocation();
+
+    fetch(reverseGeoCode, {
+      method: "GET",
+      headers: {
+        Authorization: `${apiKey}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const suggestions = data.addresses[0].formattedAddress;
+        console.log(data);
+        setCurrentLocation(suggestions);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }
+
+ useEffect(() => {
+  getCurrentLocation(); 
+ }, [LAT, LONG]);
+  
+  // Get user's cordinates
+  function getCurrentLocation() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        setLAT(latitude);
+        setLONG(longitude);
+        console.log(LAT, LONG);
+      });
+
+    } else {
+      console.log("Geolocation is not available or not allowed by the user.");
+    }
+  }
+
   return (
     <form className="w-full p-4 bg-white text-secColor">
       <div className="w-full  mb-8">
@@ -36,36 +73,33 @@ const [selectedLocation, setSelectedLocation] = useState({
           restaurants near you.
         </p>
       </div>
-      <div className="w-full cursor-pointer text-primColor border border-primColor flex items-center justify-center gap-x-2 rounded-md py-4 mb-5 hover:bg-primColor hover:text-white hover:border-none">
+      <div
+        onClick={useCurrentLocation}
+        className="w-full cursor-pointer text-primColor border border-primColor flex items-center justify-center gap-x-2 rounded-md py-4 mb-5 hover:bg-primColor hover:text-white hover:border-none"
+      >
         <Image src={location} alt="location icon" />
         <p>Use current location</p>
       </div>
       <div className="relative max-w-full">
-        <Autocomplete
-          className=""
-          onLoad={auto => (autocomplete = auto)}
-          onPlaceChanged={() => {
-            const place = autocomplete.getPlace();
-
-            if (place.geometry && place.geometry.location) {
-              const lat = place.geometry.location.lat();
-              const lng = place.geometry.location.lng();
-              setSelectedLocation({ lat, lng });
-            }
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Enter a new address"
-            className="border w-full py-4 pl-12 rounded-md"
-          />
-        </Autocomplete>
+        <input
+          type="text"
+          placeholder="Enter a new address"
+          className="border w-full py-4 pl-12 rounded-md"
+          value={currentLocation}
+          onChange={(e) => setCurrentLocation(e.target.value)}
+        />
         <Image
           src={marker}
           alt="location icon"
           className="absolute left-3 top-4"
         />
       </div>
+
+      {currentLocation.length > 0 && (
+        <button className="text-white text-center mt-3 bg-primColor rounded-md w-full py-4 text-sm">
+          Continue
+        </button>
+      )}
     </form>
   );
 };
